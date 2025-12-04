a042153e3586e17c48e7805d413f49354ca1fe09
arlebowski <adamlebowski@gmail.com>
2025-12-03 22:12:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -829,7 +829,7 @@ const AnalyticsTab = ({ loadAllFeedings }) => {
       :
         React.createElement('div', { className: "text-center text-gray-400 py-8" }, 'No data to display')
   )
-};
+}
 
 const Baby = (props) => React.createElement('svg', { ...props, xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", strokeWidth: "2", strokeLinecap: "round", strokeLinejoin: "round" }, 
   React.createElement('path', { d: "M9 12h.01" }), 
