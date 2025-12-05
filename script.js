883d2d5d05f20fcb80a3cae969aedd4dbdc4a1fa
arlebowski <adamlebowski@gmail.com>
2025-12-04 22:38:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -325,18 +325,6 @@ const App = () => {
               setLoading(false);
               return;
             }
-            
-            // Check if we need to migrate data (only once per browser)
-            const migrationFlag = localStorage.getItem('migration_complete');
-            if (!migrationFlag) {
-              const hasLocalData = localStorage.getItem('baby_weight') || 
-                                   await rtdb.ref().once('value').then(s => s.exists());
-              if (hasLocalData) {
-                await migrateLocalStorageData(userKidId);
-                localStorage.setItem('migration_complete', 'true');
-              }
-            }
-          }
           
           setKidId(userKidId);
           await firestoreStorage.initialize(userKidId);
