bdecc4d163c1407c57e5d52acbbbae7f8f37e7b9
arlebowski <adamlebowski@gmail.com>
2025-12-11 09:03:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1073,23 +1073,6 @@ const ShareIcon = (props) => React.createElement(
   React.createElement('line', { x1: "12", y1: "2", x2: "12", y2: "15" })
 );
 
-const ChevronDown = (props) => React.createElement(
-  'svg',
-  {
-    ...props,
-    xmlns: "http://www.w3.org/2000/svg",
-    width: "20",
-    height: "20",
-    viewBox: "0 0 24 24",
-    fill: "none",
-    stroke: "currentColor",
-    strokeWidth: "2",
-    strokeLinecap: "round",
-    strokeLinejoin: "round"
-  },
-  React.createElement('polyline', { points: "6 9 12 15 18 9" })
-);
-
 // Lucide-style link icon
 const LinkIcon = (props) => React.createElement(
   'svg',
@@ -1331,10 +1314,11 @@ const MainApp = ({ user, kidId, familyId, onKidChange }) => {
                 ),
                 kids.length > 1 &&
                   React.createElement(
-                    ChevronDown,
+                    'span',
                     {
-                      className: "w-4 h-4 text-gray-500 mt-[2px]"
-                    }
+                      className: "ml-1 text-sm text-gray-500"
+                    },
+                    "▾"
                   )
               ),
 
