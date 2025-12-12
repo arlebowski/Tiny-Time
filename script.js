6e6b915f50b517271bb39ec6f0d1f6e08c731c26
arlebowski <adamlebowski@gmail.com>
2025-12-11 23:52:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3586,42 +3586,6 @@ const Baby = (props) => React.createElement('svg', { ...props, xmlns: "http://ww
   React.createElement('path', { d: "M19 6.3a9 9 0 0 1 1.8 3.9 2 2 0 0 1 0 3.6 9 9 0 0 1-17.6 0 2 2 0 0 1 0-3.6A9 9 0 0 1 12 3c2 0 3.5 1.1 3.5 2.5s-.9 2.5-2 2.5c-.8 0-1.5-.4-1.5-1" })
 );
 
-const ChevronDown = (props) =>
-  React.createElement(
-    'svg',
-    {
-      ...props,
-      xmlns: 'http://www.w3.org/2000/svg',
-      viewBox: '0 0 24 24',
-      fill: 'none',
-      stroke: 'currentColor',
-      strokeWidth: '2',
-      strokeLinecap: 'round',
-      strokeLinejoin: 'round'
-    },
-    React.createElement('polyline', { points: '6 9 12 15 18 9' })
-  );
-
-const Share = (props) =>
-  React.createElement(
-    'svg',
-    {
-      ...props,
-      xmlns: 'http://www.w3.org/2000/svg',
-      viewBox: '0 0 24 24',
-      fill: 'none',
-      stroke: 'currentColor',
-      strokeWidth: '2',
-      strokeLinecap: 'round',
-      strokeLinejoin: 'round'
-    },
-    React.createElement('circle', { cx: '18', cy: '5', r: '3' }),
-    React.createElement('circle', { cx: '6', cy: '12', r: '3' }),
-    React.createElement('circle', { cx: '18', cy: '19', r: '3' }),
-    React.createElement('line', { x1: '8.59', y1: '13.51', x2: '15.42', y2: '17.49' }),
-    React.createElement('line', { x1: '15.41', y1: '6.51', x2: '8.59', y2: '10.49' })
-  );
-
 // ========================================
 // TINY TRACKER - PART 9
 // AI Chat Tab - iMessage Style
